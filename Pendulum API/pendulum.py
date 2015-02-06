a3a6b29e4a57a0f624cb2d8487bde5a3751af63d
James Irwin <irwineffect@gmail.com>
2015-02-05 20:02:10 -0800

    motor forward, backwards, stop control working

--- Pendulum API/pendulum.py
+++ Pendulum API/pendulum.py
@@ -3,6 +3,7 @@ This is the class that controls the pendulum
 '''
 import serial
 import threading
+import time
 
 class Pendulum(threading.Thread):
 	
@@ -18,18 +19,22 @@ class Pendulum(threading.Thread):
 			self.ser.port = Port
 		self.ser.open()
 		print(self.ser.name)
-		self.ser.write("0x83")
+		self.Enable()
 	def __del__(self):
 		self.Stop()
 		self.ser.close()
 	def MoveRight(self, percent):
 		speed = self.getValueFromPercent(percent)
-		self.ser.write(0x85 + ' ' + speed)
+		speed1 = chr(speed & 0x1F)
+		speed2 = chr(speed >> 5)
+		self.ser.write(chr(0x85) + speed1 + speed2)
 	def MoveLeft(self, percent):
 		speed = self.getValueFromPercent(percent)
-		self.ser.write(0x86 + ' ' + speed)
+		speed1 = chr(speed & 0x1F)
+		speed2 = chr(speed >> 5)
+		self.ser.write(chr(0x86) + speed1 + speed2)
 	def Stop(self):
-		self.ser.write("0xE0")
+		self.ser.write(chr(0xE0))
 	def getVariables():
 		self.data_lock.acquire()
 		try:
@@ -47,8 +52,24 @@ class Pendulum(threading.Thread):
 	def Reset(self):
 		while(position != 0):
 			MoveLeft(self, getValueFromPercent(10))
+
+	def Enable(self):	
+		self.ser.write(chr(0x83))
 	@staticmethod
 	def getValueFromPercent(percent):
 		return 3200 * percent / 100
-p = Pendulum(6)
-p.MoveRight(50)
+
+def tester():
+	p = Pendulum('/dev/ttyACM0')
+	while 1:
+		p.MoveRight(10)
+#		time.sleep(2)
+#		p.MoveLeft(10)
+#		time.sleep(2)
+#		p.Stop()
+#		time.sleep(1)
+#		p.Enable()		
+	
+	
+if __name__ == "__main__":
+	tester()
