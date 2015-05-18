69dbf4ef9d75bdf9261edaf8384d33f8267c4fdf
Brandon Kallaher <bkallaher@IRLL-D03>
2015-05-18 12:53:50 -0700

    Added Mounting Holes. Updated the setup.

--- Pendulum API/setup.py
+++ Pendulum API/setup.py
@@ -14,51 +14,58 @@ class Setup():
 		self.leftBrakePos = 0
 		
 def config():
-	print "Used to setup the pendulum, this will overwrite the configuration file!"
-	answer = raw_input("Is this your intended action (Y/n)?: ")
-	
-	#Exit if answer is no
-	if (answer != 'y' and answer != 'Y'):
-		print "Exiting..."
-		sys.exit()
-	
-	#Continue the program
-	
-	#Get user input
-	port = raw_input("Enter the micro-controller's port: ")
-	file_name = raw_input("Enter the file for saving: ")
-	
-	#Open the micro-controller port for reading
-	print "Initializing with port: " + port
-	uc = ucEncoder(port)
-	
-	#Open the config file for writing
-	file = open(file_name, 'w')
-	print "Opened File " + file.name + " for writing."
-	print file
-	
-	#initialize setup object
-	setup = Setup()
-	
-	#Do the setup
-	raw_input("Move the cart to the far right and press enter.")
-	uc.send_reset()
-	
-	raw_input("Move the cart to the right brake position and press enter.")
-	setup.rightBrakePos = uc.motor_count
-	
-	raw_input("Move the cart to the left brake position and press enter.")
-	setup.leftBrakePos = uc.motor_count
-	
-	#write the setup object to the file in pickle
-	pickle.dump(setup, file)
-	
-	#Close the file
-	file.close()
-	
-	file = open(file_name, 'r')
-	test = pickle.load(file)
-	print test.rightBrakePos
+    	print "Used to setup the pendulum, this will overwrite the configuration file!"
+    	answer = raw_input("Is this your intended action (Y/n)?: ")
+    	
+    	#Exit if answer is no
+    	if (answer[0] != 'y' and answer[0] != 'Y'):
+    		print "Exiting..."
+    		sys.exit()
+    	
+    	#Continue the program
+    	
+    	#Get user input
+    	port = raw_input("Enter the micro-controller's port: ")
+    	file_name = raw_input("Enter the file for saving: ")
+    	
+    	#Open the micro-controller port for reading
+    	print "Initializing with port: " + port
+    	uc = ucEncoder(port)
+    	
+    	#Open the config file for writing
+    	file = open(file_name, 'w')
+    	print "Opened File " + file.name + " for writing."
+    	print file
+    	
+    	#initialize setup object
+    	setup = Setup()
+    	
+    	#Do the setup
+    	raw_input("Move the cart to the far right and press enter.")
+    	uc.send_reset()
+    	
+    	raw_input("Move the cart to the right brake position and press enter.")
+    	setup.rightBrakePos = uc.motor_count
+    	
+    	raw_input("Move the cart to the left brake position and press enter.")
+    	setup.leftBrakePos = uc.motor_count
+    	
+    	raw_input("Move the cart around and return to the left position and press enter.")
+    	print "Left Error: " + (setup.leftBrakePos - uc.motor_count)
+     
+    	raw_input("Move the cart back to the right position and press enter.")
+    	print "Right Error: " + (setup.rightBrakePos - uc.motor_count)
+    	
+    	#write the setup object to the file in pickle
+    	pickle.dump(setup, file)
+    	
+    	#Close the file
+    	file.close()
+    	
+    	file = open(file_name, 'r')
+    	test = pickle.load(file)
+    	print "Right Brake Position: " + test.rightBrakePos
+    	print "Left Brake Position: " + test.leftBrakePos
 
 if __name__ == "__main__":
 	config()
