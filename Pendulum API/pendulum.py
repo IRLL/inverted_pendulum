d6bf735288066118d31296563ffa3d9065e8fbd6
Brandon Kallaher <brandon.kallaher@email.wsu.edu>
2015-05-19 15:48:40 -0700

    Fixed some small errors

--- Pendulum API/pendulum.py
+++ Pendulum API/pendulum.py
@@ -2,7 +2,6 @@
 '''
 This is the class that controls the pendulum
 '''
-import serial
 import threading
 import time
 import signal
@@ -11,71 +10,70 @@ import platform
 import os
 from motor import Motor
 from uc_interface import ucEncoder
-import math
 
 p = None
 LINUX = False
 WINDOWS = False
 
 class Pendulum():
-	
-	def __init__(self, motorPort, ucPort):
-		self.status = "Booting..."
-		self.motor = Motor(motorPort)
-		self.uc = ucEncoder(ucPort)
-		self.status = "Booted"
-	def __del__(self):
-		self.motor.Stop()
-	def Reset(self, start=0):
-		self.status = "resetting pendulum!"
-		speed = 19
-		left_switch = 0
-		right_switch = 0
-		while(not right_switch):
-			self.motor.MoveRight(speed)
-			switches = self.uc.getSwitches()
-			right_switch = switches[0]
-			time.sleep(.1)
-		while(not left_switch):
-			self.motor.MoveLeft(speed)
-			switches = self.uc.getSwitches()
-			left_switch = switches[1]
+    def __init__(self, motorPort, ucPort):
+        self.status = "Booting..."
+        self.motor = Motor(motorPort)
+        self.uc = ucEncoder(ucPort)
+        self.status = "Booted"
+    def __del__(self):
+        self.motor.Stop()
+    def Reset(self, start=0):
+        self.status = "resetting pendulum!"
+        speed = 19
+        left_switch = 0
+        right_switch = 0
+        while(not right_switch):
+            self.motor.MoveRight(speed)
+            switches = self.uc.getSwitches()
+            right_switch = switches[0]
+            time.sleep(.1)
+        while(not left_switch):
+            self.motor.MoveLeft(speed)
+            switches = self.uc.getSwitches()
+            left_switch = switches[1]
 		
-		#wait for arm to settle
-		current = 0
-		previous = 1
-		wait = 3
-		count = wait #must be constant for this many seconds
-		self.status = "waiting for arm to settle"
-		while count > 0: 
-			previous = current
-			time.sleep(1)
-			current = self.uc.getAngle()
-			if (previous == current):
-				count -= 1
-			else:
-				count = wait
+        #wait for arm to settle
+        current = 0
+        previous = 1
+        wait = 3
+        count = wait #must be constant for this many seconds
+        self.status = "waiting for arm to settle"
+        while count > 0: 
+            previous = current
+            time.sleep(1)
+            current = self.uc.getAngle()
+            if (previous == current):
+                count -= 1
+            else:
+                count = wait
 		
-		self.uc.send_reset()
-		time.sleep(.5)
+        self.uc.send_reset()
+        time.sleep(.5)
 
-		self.status = "moving to zero position"
-		while True:
-			position = self.uc.getPosition()
-			if (position > -12 + start):
-				break;
-			self.motor.MoveRight(speed)
-		self.motor.Stop()
+        self.status = "moving to zero position"
+        while True:
+            position = self.uc.getPosition()
+            if (position > -12 + start):
+                break;
+        self.motor.MoveRight(speed)
+        self.motor.Stop()
 		
-		self.status = "done resetting!"
-		time.sleep(1)
-	def getState(self):
-		'''
-		returns (meters, radians)
-		'''
+        self.status = "done resetting!"
+        time.sleep(1)
+        
+    def getState(self):
+        '''
+        returns (meters, radians)
+        '''
         mPose = self.uc.getXm() # meters
-		angle = self.uc.getRadians()
-		return (mPose, radians)
+        radians = self.uc.getRadians()
+        return (mPose, radians)
 	def moveRight(self, percent, threshold=30):
 		if (percent > threshold):
 			percent = threshold
@@ -94,7 +92,7 @@ class Pendulum():
 			self.moveRight(15)
 	def isTerminated(self):
 		switches = self.uc.getSwitches()
-		if switches[0] == 1 || switches[1] == 1:
+		if switches[0] == 1 or switches[1] == 1:
 			return True
 		return False
 	def getReward(self):
@@ -106,7 +104,7 @@ class Pendulum():
 		return reward
 	def stop(self):
 		self.motor.Stop()
-	def print_status():
+	def print_status(self):
 		global LINUX
 		global WINDOWS
 		
@@ -132,22 +130,22 @@ class Pendulum():
 			time.sleep(.1)
 
 	#Does all the printing for the print_status() thread		
-	def console_update():
-		stats = self.motor.getVariables()
-		switches = self.uc.getSwitches()
-		print "Pendulum Status: ", 			self.status
-		print "uC Process Status: ", 		self.uc.status
-		print "Motor Process Status: ", 	self.motor.status
-		print "Motor Controller Status: ", 	hex(stats[0])
-		print "Voltage: ", 					stats[1]
-		print "Temperature: ", 				stats[2]
-		print "Motor Speed: ", 				stats[3]
-		print "Angle: %f%c"                 %(self.uc.getAngle(), u'\xb0')
-        print "       %f rad", 			    %(self.uc.getRadians())
-		print "Position: %f meters", 		%(self.uc.getXm())
-		print "Left Switch: ", 				switches[1]
-		print "Right Switch: ", 			switches[0]
-		print ""
+	def console_update(self):
+         stats = self.motor.getVariables()
+         switches = self.uc.getSwitches()
+         print "Pendulum Status: ",               self.status
+         print "uC Process Status: ",             self.uc.status
+         print "Motor Process Status: ",          self.motor.status
+         print "Motor Controller Status: ",       hex(stats[0])
+         print "Voltage: ",                       stats[1]
+         print "Temperature: ",                   stats[2]
+         print "Motor Speed: ",                   stats[3]
+         print "Angle: %f%c"                      %(self.uc.getAngle(), u'\xb0')
+         print "       %f rad"                    %(self.uc.getRadians())
+         print "Position: %f meters"              %(self.uc.getXm())
+         print "Left Switch: ", 				switches[1]
+         print "Right Switch: ",                  switches[0]
+         print ""
 
 
 
