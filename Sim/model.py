7b471d4768ffbf5266292f05dbaef1c937ea32ad
James Irwin <irwineffect@gmail.com>
2015-09-28 10:26:10 -0700

    put all model parameters into class

--- Sim/model.py
+++ Sim/model.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
 from math import *
 
-dt = 0.01
-g = 9.81
-l = 1.0
-m = 1.0
-
-
 class Pendulum:
-	def __init__(self, start_cartx=None, start_angle=pi+pi/10, track_length=1000):
+	def __init__(self, start_cartx=None, start_angle=pi+pi/10, track_length=1000,
+			      dt=0.01, g=9.81, l=1.0, m=1.0,
+				  cfriction=.008, pfriction=0.005):
+
 		self.track_length = track_length
+
+		self.dt = dt
+		self.g = g
+		self.l = l
+		self.m = m
+
+		self.cfriction = cfriction
+		self.pfriction = pfriction
+
 		self.reset(start_cartx, start_angle)
 
 	def reset(self, start_cartx=None, start_angle=pi+pi/10):
@@ -23,26 +29,26 @@ class Pendulum:
 		self.cartx = start_cartx
 		self.carty = 0
 		self.cartx_vel = 0
-		self.massx = self.cartx + l * sin(self.angle0)
-		self.massy = self.carty + l * cos(self.angle0)
+		self.massx = self.cartx + self.l * sin(self.angle0)
+		self.massy = self.carty + self.l * cos(self.angle0)
 
 
 	def update(self, control):
 		#calculate new angle
 		self.angle = atan2(self.massx - self.cartx, self.massy - self.carty)
-		d_velocity = -g * sin(self.angle) * dt / l 
+		d_velocity = -self.g* self.m * sin(self.angle) * self.dt / self.l 
 		self.velocity += d_velocity #add acceleration due to gravity
-		self.velocity += -self.velocity * .005 #add acceleration due to friction 
-		d_angle = dt * self.velocity
+		self.velocity += -self.velocity * self.pfriction #add acceleration due to friction 
+		d_angle = self.dt * self.velocity
 		self.angle = self.angle + d_angle
-		self.massx = self.cartx + l * sin(self.angle)
-		self.massy = self.carty + l * cos(self.angle)
+		self.massx = self.cartx + self.l * sin(self.angle)
+		self.massy = self.carty + self.l * cos(self.angle)
 
 		#calculate new cart position
-		d_cartx_vel = dt * control
+		d_cartx_vel = self.dt * control
 		self.cartx_vel += d_cartx_vel #add acceleration from input force
-#self.cartx_vel += -self.cartx_vel * .008 #add acceleration due to friction 
-		dcartx = dt * self.cartx_vel
+		self.cartx_vel += -self.cartx_vel * self.cfriction #add acceleration due to friction 
+		dcartx = self.dt * self.cartx_vel
 		self.cartx = self.cartx + dcartx
 
 		#limit cart position
