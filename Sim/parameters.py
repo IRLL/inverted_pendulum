f8839396779222ce5ff0523af0bcfd0d73fa87f8
Anthony La <anthony.la@wsu.edu>
2015-10-07 09:02:04 -0700

    First pass at Sarsa with NN - Not learning.

---
+++ Sim/parameters.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Learning parameters and such.
+"""
+
+# Learning rate.
+ALPHA = 0.001
+
+# Epsilon greedy random action selection rate.
+EPSILON = 0.01
+
+# Eligibility trace decay.
+LAMBDA = 0.9
