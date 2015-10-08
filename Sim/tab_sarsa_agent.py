1d504ccc0667801135bf830d15f07fc64730785c
James Irwin <irwineffect@gmail.com>
2015-10-07 17:13:30 -0700

    started tabular sarsa agent

---
+++ Sim/tab_sarsa_agent.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class agent:
+    def __init__(self):
+        pass
+
+
+    def update_values():
+        pass
+
+    def get_action(x, angle, dx, dangle, edge):
+        self.update_values()
+        
+        if(self.should_explore()): #epsilon-greedy explore
+            pass
+        else: #exploit
+            #action = 
+
+
+
+        return action
+        
+
+def tester():
+    pass
+
+if __name__ == "__main__":
+    tester()
