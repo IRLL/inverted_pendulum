738c823e91e111397d860cea05ab34f3bb8c9274
Brandon Kallaher <brandonj@kallaher.org>
2016-03-20 12:25:45 -0700

    Added base files to the package

---
+++ ROS Integration/src/inverted_pendulum/src/MotorSerial.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import rospy
+
+from std_msgs.msg import Int32
+
+
+rospy.init_node('MotorSerial')
+
+sub = rospy.Subscriber('/motor/cmd', Int32)
+pub = rospy.Publisher('/motor/info', Int32)
+
+while not rospy.is_shutdown():
+    
