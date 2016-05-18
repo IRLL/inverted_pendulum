45641f5b939921cbba9e80603eb30e9cacd5ef39
Brandon Kallaher <brandonj@kallaher.org>
2016-05-17 18:25:13 -0700

    Made the Status a class for readability

--- ROSIntegration/src/inverted_pendulum/scripts/StatusDisplay.py
+++ ROSIntegration/src/inverted_pendulum/scripts/StatusDisplay.py
@@ -16,78 +16,83 @@ from inverted_pendulum.msg import PendulumPose
 from inverted_pendulum.msg import MotorInfo
 from inverted_pendulum.msg import Cmd
 
-fps = 20
+class Status():
+    def __init__(self):
+        self.fps = 20
 
-cmd = 0.0
-x = 0.0
-theta = 0.0
-vel = 0.0
-right = False
-left = False
-errorStatus = 0x0
-SerialError = 0x0
-limitStatus = 0
-targetSpeed = 0
-speed = 0
-brakeAmt = 0
-vin = 0
-temp = 0
+        self.cmd = 0.0
+        self.x = 0.0
+        self.theta = 0.0
+        self.vel = 0.0
+        self.tDot = 0.0
+        self.right = False
+        self.left = False
+        self.errorStatus = 0x0
+        self.SerialError = 0x0
+        self.limitStatus = 0
+        self.targetSpeed = 0
+        self.speed = 0
+        self.brakeAmt = 0
+        self.vin = 0
+        self.temp = 0
 
-#define the callbacks
-def info_callback(data):
-    global errorStatus, SerialError, limitStatus, targetSpeed, speed, \
-            brakeAmt, vin, temp
-    errorStatus = data.errorStatus
-    SerialError = data.SerialError
-    limitStatus = data.limitStatus
-    targetSpeed = data.targetSpeed
-    speed = data.speed
-    brakeAmt = data.brakeAmt
-    vin = data.vin
-    temp = data.temp
+    def redraw(self, time):
+        os.system('clear')
+        print "CMD: ", self.cmd
+        print "Sensors:"
+        print "  X: ", self.x
+        print u"  x\u0307: ", self.vel
+        print u"  \u03B8: ", self.theta
+        print u"  \u03B8'", "Velocity: ", self.tDot
+        print "  Limits:"
+        print "    Right: ", self.right
+        print "    Left: ", self.left
+        print "Motor Status:"
+        print "  Error Status: ", hex(self.errorStatus)
+        print "  Serial Error: ", hex(self.SerialError)
+        print "  Limit Status: ", self.limitStatus
+        print "  Target Speed: ", self.targetSpeed
+        print "  Speed: ", self.speed
+        print "  Brake Amount: ", self.brakeAmt
+        print "  Voltage: ", self.vin
+        print "  Temperature: ", self.temp
 
-def cmd_callback(data):
-    global cmd
-    cmd = data.cmd
+    def info_callback(self, data):
+        self.errorStatus = data.errorStatus
+        self.SerialError = data.SerialError
+        self.limitStatus = data.limitStatus
+        self.targetSpeed = data.targetSpeed
+        self.speed = data.speed
+        self.brakeAmt = data.brakeAmt
+        self.vin = data.vin
+        self.temp = data.temp
 
-def sensor_callback(data):
-    global x, theta, vel, right, left
-    x = data.x
-    theta = data.theta
-    vel = data.vel
-    right = data.rightLim
-    left = data.leftLim
+    def cmd_callback(self, data):
+        self.cmd = data.cmd
 
-def redraw(time):
-    os.system('clear')
-    print "CMD: ", cmd
-    print "Sensors:"
-    print "  X: ", x
-    print "  Theta: ", theta
-    print "  Velocity: ", vel
-    print "  Limits:"
-    print "    Right: ", right
-    print "    Left: ", left
-    print "Motor Status:"
-    print "  Error Status: ", hex(errorStatus)
-    print "  Serial Error: ", hex(SerialError)
-    print "  Limit Status: ", limitStatus
-    print "  Target Speed: ", targetSpeed
-    print "  Speed: ", speed
-    print "  Brake Amount: ", brakeAmt
-    print "  Voltage: ", vin
-    print "  Temperature: ", temp
+
+    def sensor_callback(self, data):
+        self.x = data.x
+        self.vel = data.xDot
+        self.theta = data.theta
+        self.tDot = data.thetaDot
+        self.right = data.rightLim
+        self.left = data.leftLim
 
 #initialize the ros node
-rospy.init_node('StatusDisplay')
+status = Status()
 
-motor_info_sub = rospy.Subscriber('/motor/info', MotorInfo, info_callback)
-motor_cmd_sub = rospy.Subscriber('/cmd', Cmd, cmd_callback)
-sensor_sub = rospy.Subscriber('/sensors', PendulumPose, sensor_callback)
 
-rospy.Timer(rospy.Duration(1.0/fps), redraw)
+if __name__ == "__main__":
+    rospy.init_node('StatusDisplay')
 
-rospy.spin()
+    motor_info_sub = rospy.Subscriber('/motor/info', MotorInfo, status.info_callback)
+    motor_cmd_sub = rospy.Subscriber('/cmd', Cmd, status.cmd_callback)
+    sensor_sub = rospy.Subscriber('/sensors', PendulumPose, status.sensor_callback)
+
+    rospy.Timer(rospy.Duration(1.0/status.fps), status.redraw)
+
+    rospy.spin()
 
 # while not rospy.is_shutdown():
 #     os.system('clear')
