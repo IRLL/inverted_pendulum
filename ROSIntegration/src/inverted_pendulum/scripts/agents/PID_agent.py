36caab03023e1f7fabe6ef65a0a37d49766bde54
James Irwin <irwineffect@gmail.com>
2016-08-27 16:35:18 -0700

    tuned pid agent

--- ROSIntegration/src/inverted_pendulum/scripts/agents/PID_agent.py
+++ ROSIntegration/src/inverted_pendulum/scripts/agents/PID_agent.py
@@ -2,27 +2,57 @@
 
 import rospy
 from inverted_pendulum.msg import PendulumPose, Cmd, PID_state
+from std_msgs.msg import Header
 import threading
 import math
+
+class PID():
+    def __init__(self, parameter_dict):
+        self.kp = parameter_dict['p']
+        self.ki = parameter_dict['i']
+        self.kd = parameter_dict['d']
+        self.error_integral = 0
+        self.prev_error = 0
+        self.derivative = 0
+
+    def update(self, error):
+        self.derivative =  error - self.prev_error
+        self.prev_error = error
+        self.error_integral += error
+
+        return  self.kp * error + \
+                  self.ki * self.error_integral + \
+                  self.kd * self.derivative
+
+    def reset(self):
+        self.error_integral = 0
+        self.prev_error = 0
+        self.derivative = 0
+
+    def get_state(self):
+        return self.prev_error, self.error_integral, self.derivative
+
+
 
 class Node():
     def __init__(self):
         self.sensor_sub = rospy.Subscriber('sensors', PendulumPose, self.sensor_callback) 
         self.cmd_pub = rospy.Publisher('cmd', Cmd, queue_size=1)
-        self.pid_pub = rospy.Publisher('pid', PID_state, queue_size=1)
+        self.angle_pid_state_pub = rospy.Publisher('pid/angle', PID_state, queue_size=1)
+        self.cart_pid_state_pub = rospy.Publisher('pid/cart', PID_state, queue_size=1)
         self.sensor_lock = threading.Lock()
         self.sensor_data = list()
 
-        pid_parameters = rospy.get_param('pendulum/pid')
-        
+
+        pid_parameters = rospy.get_param('pendulum/pid/')
         self.rate = rospy.Rate(pid_parameters['update_frequency'])
-        self.kp = pid_parameters['p']
-        self.ki = pid_parameters['i']
-        self.kd = -pid_parameters['d']
-        self.xpos_weight = pid_parameters['xpos_weight']
-        self.angle_weight = pid_parameters['angle_weight']
-        self.error_integral = 0
-        self.prev_error = 0
+        angle_pid = pid_parameters['angle']
+        cart_pid = pid_parameters['cart']
+
+        self.angle_pid = PID(pid_parameters['angle'])
+        self.cart_pid = PID(pid_parameters['cart'])
+
+
 
     def sensor_callback(self, data):
         self.sensor_lock.acquire()
@@ -45,26 +75,26 @@ class Node():
 
         cmd = Cmd()
         pid_state = PID_state()
-        cmd.header.stamp = rospy.Time.now()
-        pid_state.header.stamp = cmd.header.stamp
-        angle = current_sensors.theta
+        time = rospy.Time.now()
+        cmd.header.stamp = time 
         
-        error = -angle*self.angle_weight + -current_sensors.x*self.xpos_weight
-        derivative = self.prev_error - error
-        self.prev_error = error
-        self.error_integral += error
 
-        cmd.cmd = self.kp * error + \
-                  self.ki * self.error_integral + \
-                  self.kd * derivative
+        angle_correction = self.angle_pid.update(-current_sensors.theta)
+        cart_correction = self.cart_pid.update(-current_sensors.x)
+
+        cmd.cmd = angle_correction + cart_correction
 
         self.cmd_pub.publish(cmd)
 
-        pid_state.error = error
-        pid_state.error_integral = self.error_integral
-        pid_state.error_derivative = derivative
-        self.pid_pub.publish(pid_state)
+        pid_state.header = Header()
+        pid_state.header.stamp = time
+        pid_state.error, pid_state.error_integral, pid_state.error_derivative = self.angle_pid.get_state()	
+        self.angle_pid_state_pub.publish(pid_state)
 
+        pid_state.header = Header()
+        pid_state.header.stamp = time
+        pid_state.error, pid_state.error_integral, pid_state.error_derivative = self.cart_pid.get_state()	
+        self.cart_pid_state_pub.publish(pid_state)
         
         
         
