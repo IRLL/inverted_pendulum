c4b006bd8fe2c8ceffc21407c9b82410b7fa3f57
James Irwin <irwineffect@gmail.com>
2016-09-02 12:49:06 -0700

    removed old files

--- Pendulum API/loopback.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#creates two ports that are connected to each other for testing
-socat -d -d pty,raw,echo=0 pty,raw,echo=0
--- Sim/run_sarsa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./simulator.py --episodes 200 --nogui --track_length 20 --fail_angle 90 tab_sarsa_agent.py
