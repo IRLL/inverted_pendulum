b9c7a937e80cefd59ec536d9feb2ce1bfc7183f1
James Irwin <irwineffect@gmail.com>
2016-10-06 14:04:37 -0700

    removed dead code from arduino sensors
    limit switch functionality is handled by the motor controller, not the sensors

--- src/arduino_sensors/arduino_sensors.ino
+++ src/arduino_sensors/arduino_sensors.ino
@@ -4,10 +4,7 @@
 
 
 #define LIN_POS_PIN A0
-#define LEFT_LIMIT_SW_PIN 10
-#define RIGHT_LIMIT_SW_PIN 11
 #define MSG_SIZE 3
-#define FREQUENCY 10
 
 ros::NodeHandle roshandle;
 std_msgs::Int16MultiArray m_msg;
@@ -52,19 +49,15 @@ void loop() {
 	raw_angle = ((rx[3] << 8 | rx[4]));
 
 	int raw_position = analogRead(LIN_POS_PIN);
-	byte left_switch = digitalRead(LEFT_LIMIT_SW_PIN);
-	byte right_switch = digitalRead(RIGHT_LIMIT_SW_PIN);
-	byte switch_bitfield = 0;
-	switch_bitfield |= left_switch ? 0x2 : 0x0;
-	switch_bitfield |= right_switch ? 0x1 : 0x0;
 
 
 
 	m_msg.data[0] = raw_angle;
 	m_msg.data[1] = raw_position;
-	m_msg.data[2] = (status_bits << 8) | switch_bitfield;
+	m_msg.data[2] = (status_bits << 8);
 
 	m_publisher.publish(&m_msg);
 	roshandle.spinOnce();
+  delay(10);
 }
 
